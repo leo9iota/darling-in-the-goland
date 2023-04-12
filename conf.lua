471e097dba13bdf9ef1a9a8300f77afa99fbef2c
leo9iota <leon20.zh@gmail.com>
2023-04-12 13:22:07 +0200

    Add config

--- conf.lua
+++ conf.lua
@@ -1,3 +1,8 @@
 function love.conf(t)
-    
+    t.title = "Platformer"
+    t.version = "11.4"
+    t.console = true
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 0
 end
