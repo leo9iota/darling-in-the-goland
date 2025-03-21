3c63d73619ba07d1eccade99a3a8a8fceda30f48
leo9iota <leo9iota@gmail.com>
2025-03-21 22:24:37 +0100

    Add implementation for enemy

--- Enemy.lua
+++ Enemy.lua
@@ -1,46 +1,135 @@
-local Stone = {
-    image = love.graphics.newImage("assets/stone.png") -- NOTE: Load image here and not within the "new()" method to avoid unnecessary memory usage
-}
-Stone.__index = Stone
+local Enemy = {}
+Enemy.__index = Enemy
+local Player = require("Player")
 
-Stone.width = Stone.image:getWidth()
-Stone.height = Stone.image:getHeight()
+local ActiveEnemies = {}
 
-local ActiveStones = {}
+function Enemy.removeAll()
+    for i, v in ipairs(ActiveEnemies) do v.physics.body:destroy() end
 
-function Stone.new(x, y)
-    local stone = setmetatable({}, Stone)
-    stone.x = x
-    stone.y = y
-    stone.rotation = 0
-
-    stone.physics = {}
-    stone.physics.body = love.physics.newBody(World, stone.x, stone.y, "dynamic")
-    stone.physics.shape = love.physics.newRectangleShape(stone.width, stone.height)
-    stone.physics.fixture = love.physics.newFixture(stone.physics.body, stone.physics.shape)
-    stone.physics.body:setMass(25)
-    table.insert(ActiveStones, stone)
+    ActiveEnemies = {}
 end
 
-function Stone:update(dt)
+function Enemy.new(x, y)
+    local enemy = setmetatable({}, Enemy)
+    enemy.x = x
+    enemy.y = y
+    enemy.offsetY = -8
+    enemy.rotation = 0
+
+    enemy.speed = 100
+    enemy.speedMod = 1
+    enemy.xVel = enemy.speed
+
+    enemy.rageCounter = 0
+    enemy.rageTrigger = 3
+
+    enemy.damage = 1
+
+    enemy.state = "walk" -- Default state
+
+    enemy.animation = {
+        timer = 0,
+        rate = 0.1
+    }
+    enemy.animation.run = {
+        total = 4,
+        current = 1,
+        img = Enemy.runAnim
+    }
+    enemy.animation.walk = {
+        total = 4,
+        current = 1,
+        img = Enemy.walkAnim
+    }
+    enemy.animation.draw = enemy.animation.walk.img[1]
+
+    enemy.physics = {}
+    enemy.physics.body = love.physics.newBody(World, enemy.x, enemy.y, "dynamic")
+    enemy.physics.body:setFixedRotation(true)
+    enemy.physics.shape = love.physics.newRectangleShape(enemy.width * 0.4, enemy.height * 0.75)
+    enemy.physics.fixture = love.physics.newFixture(enemy.physics.body, enemy.physics.shape)
+    enemy.physics.body:setMass(25)
+    table.insert(ActiveEnemies, enemy)
+end
+
+function Enemy.loadAssets()
+    Enemy.runAnim = {}
+    for i = 1, 4 do Enemy.runAnim[i] = love.graphics.newImage("assets/enemy/run/" .. i .. ".png") end
+
+    Enemy.walkAnim = {}
+    for i = 1, 4 do Enemy.walkAnim[i] = love.graphics.newImage("assets/enemy/walk/" .. i .. ".png") end
+
+    Enemy.width = Enemy.runAnim[1]:getWidth()
+    Enemy.height = Enemy.runAnim[1]:getHeight()
+end
+
+function Enemy:update(dt)
     self:syncPhysics()
+    self:animate(dt)
 end
 
-function Stone:syncPhysics()
+function Enemy:incrementRage()
+    self.rageCounter = self.rageCounter + 1
+    if self.rageCounter > self.rageTrigger then
+        self.state = "run"
+        self.speedMod = 3
+        self.rageCounter = 0
+    else
+        self.state = "walk"
+        self.speedMod = 1
+    end
+end
+
+function Enemy:flipDirection()
+    self.xVel = -self.xVel
+end
+
+function Enemy:animate(dt)
+    self.animation.timer = self.animation.timer + dt
+    if self.animation.timer > self.animation.rate then
+        self.animation.timer = 0
+        self:setNewFrame()
+    end
+end
+
+function Enemy:setNewFrame()
+    local anim = self.animation[self.state]
+    if anim.current < anim.total then
+        anim.current = anim.current + 1
+    else
+        anim.current = 1
+    end
+    self.animation.draw = anim.img[anim.current]
+end
+
+function Enemy:syncPhysics()
     self.x, self.y = self.physics.body:getPosition()
-    self.rotation = self.physics.body:getAngle()
+    self.physics.body:setLinearVelocity(self.xVel * self.speedMod, 100)
 end
 
-function Stone:draw()
-    love.graphics.draw(self.image, self.x, self.y, self.rotation, self.scaleX, 1, self.width / 2, self.height / 2)
+function Enemy:draw()
+    local scaleX = 1
+    if self.xVel < 0 then scaleX = -1 end
+    love.graphics.draw(self.animation.draw, self.x, self.y + self.offsetY, self.rotation, scaleX, 1, self.width / 2, self.height / 2)
 end
 
-function Stone.updateAll(dt)
-    for i, instance in ipairs(ActiveStones) do instance:update(dt) end
+function Enemy.updateAll(dt)
+    for i, enemy in ipairs(ActiveEnemies) do enemy:update(dt) end
 end
 
-function Stone.drawAll()
-    for i, instance in ipairs(ActiveStones) do instance:draw() end
+function Enemy.drawAll()
+    for i, enemy in ipairs(ActiveEnemies) do enemy:draw() end
 end
 
-return Stone
+function Enemy.beginContact(a, b, collision)
+    for i, enemy in ipairs(ActiveEnemies) do
+        if a == enemy.physics.fixture or b == enemy.physics.fixture then
+            if a == Player.physics.fixture or b == Player.physics.fixture then Player:takeDamage(enemy.damage) end
+            enemy:incrementRage()
+            enemy:flipDirection()
+        end
+    end
+end
+
+return Enemy
