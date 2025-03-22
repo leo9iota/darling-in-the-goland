cf2908c782e2440b5c629feb25c63f1c63894377
leo9iota <leo9iota@gmail.com>
2025-03-22 19:01:37 +0100

    Update location of entity and map loading functionality

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 love.graphics.setDefaultFilter("nearest", "nearest") -- Set filter to have pixel esthetic
 
--- Import STI library to import maps from Tiled
-local STI = require("sti")
+local STI = require("sti") -- Import STI library to import maps from Tiled
 
+local Map = require "Map"
 local Camera = require "Camera"
 local Player = require "Player"
 local Coin = require "Coin"
@@ -14,30 +14,7 @@ local Enemy = require "Enemy"
 -- math.randomseed(os.time()) -- Generate truly random numbers
 
 function love.load()
-    local tileSizeInPixels = 16
-
-    --[[
-        The STI library relies on the open-source Box2D physics engine. With the "newWorld()"
-        function call we get gravity. The "initBox2D()" function loads all layers and
-        objects that have the custom property "collidable" into the new world.
-    ]]
-    Map = STI("maps/map-1.lua", {"box2d"})
-    World = love.physics.newWorld(0, 2000)
-
-    --[[
-        This function takes 4 functions as arguments but the last two are optional. The
-        "beginContact" callback function gets called when two fixtures collide and the
-        "endContact" callback functions gets called when two fixtures that we are calling
-        no longer collide.
-    ]]
-    World:setCallbacks(beginContact, endContact)
-
-    Map:initBox2D(World)
-
-    Map.layers.solid.visible = false -- Hide "solid" layer from Tiled
-    Map.layers.entity.visible = false -- Hide "entity" layer from Tiled
-
-    MapWidth = Map.layers.ground.width * tileSizeInPixels -- Prevent camera to go out of bounds on right side
+    Map:load()
 
     background = love.graphics.newImage("assets/background.png")
     GUI:load()
@@ -45,8 +22,6 @@ function love.load()
     Enemy.loadAssets()
 
     Player:load()
-
-    spawnEntities()
 end
 
 --[[
@@ -128,26 +103,3 @@ function endContact(fixtureA, fixtureB, collision)
     Player:endContact(fixtureA, fixtureB, collision)
 end
 
---[[ 
-    Loop through "object" table to get all the entities.
-    NOTE: Different origin points for circles and rectangles in Tiled.
-        - Rectangle: top left
-        - Circle: center
-]]
-function spawnEntities()
-    for i, v in ipairs(Map.layers.entity.objects) do
-        --[[ 
-            NOTE: Since the latest update in Tiled, the field isn't called "type" anymore, its called "class", but since 
-            I use the old version of the STI lib, its still "type", or until I update the lib.
-        ]]
-        if v.type == "spikes" then
-            Spike.new(v.x + v.width / 2, v.y + v.height / 2) -- The origin point in Tiled is the top left corner, but origin point of the physics module is the center, which means we need an offset 
-        elseif v.type == "stone" then
-            Stone.new(v.x + v.width / 2, v.y + v.height / 2)
-        elseif v.type == "enemy" then
-            Enemy.new(v.x + v.width / 2, v.y + v.height / 2)
-        elseif v.type == "coin" then
-            Coin.new(v.x, v.y) -- In Tiled a circle's origin point is in the center, so we can just pass x and y without offsets
-        end
-    end
-end
