d300e78ae4ca03a4866f372f9acc487ab6d4e82a
leo9iota <leo9iota@gmail.com>
2025-03-29 15:37:35 +0100

    Add configuration options for window resizing, fullscreen, icon, and module optimization

--- conf.lua
+++ conf.lua
@@ -5,4 +5,25 @@ function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
     t.window.vsync = 0
+    --[[ 
+        Allow resizing of game window, if the window is resizable, you can set minimum dimensions:
+    ]]
+    -- t.window.resizable = false
+    -- t.window.minwidth = 800
+    -- t.window.minheight = 600
+    --[[ 
+        Set to fullscreen
+    ]]
+    t.window.fullscreen = false
+    --[[ 
+        Set a custom icon for the game window
+    ]]
+    -- t.window.icon = "assets/icons/game-icon.png"
+    --[[ 
+        Disable unused modules to optimize performance
+    ]]
+    -- t.modules.joystick = false
+    -- t.modules.physics = true
+    -- t.modules.audio = true
+    -- t.modules.video = false
 end
