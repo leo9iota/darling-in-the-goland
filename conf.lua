16ed984cb63c977b127119974fce1560388997c6
leo9iota <leo9iota@gmail.com>
2025-03-29 15:31:40 +0100

    Update game title in configuration

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
-    t.title = "Platformer"  -- Title of the game window
-    t.version = "11.4"      -- LÖVE 2D version this game is made for
-    t.console = true        -- Enable console for debug
-    t.window.width = 1280   -- Window width in pixel
-    t.window.height = 720   -- Window height in pixel
-    t.window.vsync = 0      -- Disable vsync
+    t.title = "Darling in the Goland"
+    t.version = "11.4"
+    t.console = true
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 0
 end
