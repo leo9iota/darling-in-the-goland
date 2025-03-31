5e0b5b3e3d5eb6af97f6b8934c818ecb20ef2156
leo9iota <leo9iota@gmail.com>
2025-03-31 10:32:07 +0200

    Update debug console setting

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.title = "Darling in the Goland"
     t.version = "11.4"
-    t.console = true
+    t.console = false
     t.window.width = 1280
     t.window.height = 720
     t.window.vsync = 0
